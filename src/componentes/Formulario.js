fe21c38d8cbb11fe8000c0927a507caf60fb9ad8
Karlafgl <karlafgl@gmail.com>
2019-08-10 11:41:26 -0600

    first commit

---
+++ src/componentes/Formulario.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const Formulario = () =>{
+  return(
+    <form>
+      <label>Formulario</label>
+      <input type='text' name=''/>
+    </form>
+  )
+}
+
+export default Formulario;
