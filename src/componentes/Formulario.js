e641dea650eb70ae1375ee22a156c9523ebfa00b
karla <karla@Threde-LLC.local>
2019-08-25 14:02:35 -0600

    adding css styles

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,26 +1,66 @@
 import React, { Component } from 'react';
 
-const Formulario = () =>{
-  return(
-    <form>
-      <div>
-        <label>Cantidad Prestamo:</label>
-        <input type='text' name='cantidad' className="u-full-width" placeholder="Ejemplo: 3000"/>
-      </div>
-      <div>
-        <select name="plazo" className="u-full-width">
-        <option value="">Seleccionar</option>
-          <option value="3">3 Meses</option>
-          <option value="6">6 Meses</option>
-          <option value="12">12 Meses</option>
-          <option value="24">24 Meses</option>
-        </select>
-      </div>
-      <div>
-        <input value="Calcular" placeholder="" type="submit" className="u-full-width button-primary"/>
-      </div>
-    </form>
-  )
+class Formulario extends Component {
+  state = {
+    cantidad: '',
+    plazo:''
+  }
+
+  actualizarState = (e) => {
+    //leer los datos del formulario
+    const {name, value} = e.target
+
+   //actualizar el state
+   this.setState({
+     [name]: Number(value)
+   })
+  }
+
+  habilitarSubmit = () => {
+    //aplicar destructuting para obtener valores
+    const {cantidad, plazo} = this.state
+
+    //Leer las variables
+    const noValido = !cantidad || !plazo
+    console.log(noValido)
+    //retornar respuesta
+    return noValido
+
+  }
+
+  render(){
+
+    const {cantidad} = this.state
+    console.log(cantidad)
+
+    return(
+      <form>
+        <div>
+          <label>Cantidad Prestamo:</label>
+          <input 
+                onChange={this.actualizarState}
+                type='text' 
+                name='cantidad' 
+                className="u-full-width" 
+                placeholder="Ejemplo: 3000"/>
+        </div>
+        <div>
+          <label>Plazo para pagar:</label>
+          <select onChange={this.actualizarState} name="plazo" className="u-full-width">
+            <option value="">Seleccionar</option>
+            <option value="3">3 Meses</option>
+            <option value="6">6 Meses</option>
+            <option value="12">12 Meses</option>
+            <option value="24">24 Meses</option>
+          </select>
+        </div>
+        <div>
+          <input disabled={this.habilitarSubmit()} value="Calcular" placeholder="" type="submit" className="u-full-width button-primary"/>
+        </div>
+      </form>
+    )
+  }
 }
+ 
 
 export default Formulario;
