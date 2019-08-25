5e0fced9d4e825fe89db39bdbe99ba067f8558ac
karla <karla@Threde-LLC.local>
2019-08-25 12:40:23 -0600

    creating form component

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -3,8 +3,22 @@ import React, { Component } from 'react';
 const Formulario = () =>{
   return(
     <form>
-      <label>Formulario</label>
-      <input type='text' name=''/>
+      <div>
+        <label>Cantidad Prestamo:</label>
+        <input type='text' name='cantidad' className="u-full-width" placeholder="Ejemplo: 3000"/>
+      </div>
+      <div>
+        <select name="plazo" className="u-full-width">
+        <option value="">Seleccionar</option>
+          <option value="3">3 Meses</option>
+          <option value="6">6 Meses</option>
+          <option value="12">12 Meses</option>
+          <option value="24">24 Meses</option>
+        </select>
+      </div>
+      <div>
+        <input value="Calcular" placeholder="" type="submit" className="u-full-width button-primary"/>
+      </div>
     </form>
   )
 }
